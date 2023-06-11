2b15cbfd87b7b96247505ae9b880b4cadc2afaa6
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-06-12 02:48:34 +0900

    up

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { App } from '@slack/bolt';
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  console.log(message);
+  say(`Hey there <@${message}>!`);
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
